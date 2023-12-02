56f2241c31f19bd24fdafd82d645a666b31b8fd8
LuigiMottola <sputnick427@gmail.com>
2023-12-02 10:43:43 +0100

    Termine esercizio

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        Student student1 = new Student("Umberto", "Cavalcanti", 1313);
+
+        System.out.println("Student1 = " + student1);
+    }
+}
